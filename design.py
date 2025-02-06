1fc982cd143605a1badb820472709c4ba17067e0
Abena <priscillaabenaaboagye@gmail.com>
2025-02-06 12:52:44 +0000

    Update design.py

--- design.py
+++ design.py
@@ -20,8 +20,9 @@ my_graph = {
 
 def shortest_path(graph, start):
     unvisited = list(graph)
-    distances = {}
+    distances = {node: 0 if node == start else float("inf") for node in graph}
     paths = {node: [] for node in graph}
+    paths[start].append(start)
 
-    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
 shortest_path(my_graph, "A")

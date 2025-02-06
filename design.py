8aeca823b906528dcc74d4948ed517594e888f13
Abena <priscillaabenaaboagye@gmail.com>
2025-02-06 11:49:43 +0000

    Creating a graph

--- design.py
+++ design.py
@@ -5,3 +5,20 @@ copper = {
 }
 
 copper["species"] = "Cavia porcellus"
+
+del copper["age"]
+
+for i, j in copper.items():
+    print(i, j)
+
+my_graph = {
+    "A": [("B", 3), ("D", 1)],
+    "B": [("A", 3), ("C", 4)],
+    "C": [("B", 4), ("D", 7)],
+    "D": [("A", 1), ("C", 7)]
+}
+
+def shortest_path(graph, start):
+    unvisited = []
+    for i in unvisited:
+        unvisited.append(i)
